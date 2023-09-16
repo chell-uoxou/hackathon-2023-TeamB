c9eee427ea4b42d3c94b17e64ea4daad442ba71c
chelluoxou <chelluoxou@gmail.com>
2023-09-17 01:53:40 +0900

    fix: 画像の削除

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ export default function Home() {
   return (
     <main>
       <h1>this is sharedragon</h1>
-      <Image src="/next.svg" width={150} height={150} alt="next logo"></Image>
     </main>
   )
 }
