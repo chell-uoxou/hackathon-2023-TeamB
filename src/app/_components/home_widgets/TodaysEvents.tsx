66ae56f020fda6f5ba7bd7536fbd192840290109
maezawa-lab <maezawadesu728@gmail.com>
2023-09-18 02:35:51 +0900

    feat: ホーム画面、今日の予定

---
+++ src/app/_components/home_widgets/TodaysEvents.tsx
@@ -0,0 +1,30 @@
+import { Avatar, AvatarGroup, Box, Card, HStack, Text } from '@chakra-ui/react'
+
+export type avatarData = {
+  name: string
+  avatarUrl: string
+}
+
+type Props = {
+  avatars: avatarData[]
+  eventTitle: string
+}
+
+export default function TodaysEvents(props: Props) {
+  return (
+    <Card width={'100%'} padding={3}>
+      <HStack>
+        <Box w={'200px'}>
+          <AvatarGroup size="sm" max={3}>
+            {props.avatars.map((e: avatarData) => {
+              return <Avatar name={e.name} src={e.avatarUrl} />
+            })}
+          </AvatarGroup>
+        </Box>
+        <Text fontSize={'16px'} fontWeight={''}>
+          {props.eventTitle}
+        </Text>
+      </HStack>
+    </Card>
+  )
+}
